2328595310dc6e5959ef40d434d327361b1a3ddd
Sonia <Sonia@vagvlan536.391.wlan.library.upenn.edu>
2015-03-26 17:18:31 -0400

    Added registration page

--- Instaderm/app/src/main/java/cis350/instaderm/RegistrationActivity.java
+++ Instaderm/app/src/main/java/cis350/instaderm/RegistrationActivity.java
@@ -1,7 +1,37 @@
 package cis350.instaderm;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+
 /**
  * Created by Sonia on 3/18/15.
  */
-public class RegistrationActivity {
-}
+public class RegistrationActivity extends Activity implements View.OnClickListener{
+    Spinner genderSelection;
+    Spinner countrySelection;
+    Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+
+        // Access the spinner
+        genderSelection = (Spinner) findViewById(R.id.gender_spinner);
+        countrySelection = (Spinner) findViewById(R.id.country_spinner);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        // Get the spinner selection
+        int gender = genderSelection.getSelectedItemPosition();
+
+
+    }
+    }
