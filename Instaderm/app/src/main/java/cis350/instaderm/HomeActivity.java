45e57461228a5afc87b1141fb5340acc7b52b784
annawuyang <annayang@wharton.upenn.edu>
2015-03-26 19:23:06 -0400

    3/26/15 Anna
    Linked Home page to Login page

--- Instaderm/app/src/main/java/cis350/instaderm/HomeActivity.java
+++ Instaderm/app/src/main/java/cis350/instaderm/HomeActivity.java
@@ -1,7 +1,92 @@
 package cis350.instaderm;
 
-/**
- * Created by Sonia on 3/18/15.
- */
-public class HomeActivity {
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Button;
+
+
+public class HomeActivity extends ActionBarActivity {
+
+    Button submitButton;
+    Button viewCollectionButton;
+    Button searchButton;
+    Button logoutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        submitButton = (Button) findViewById(R.id.submitCaseButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SubmitActivity.class);
+                String temp = "THE CLICK WORKED";
+                Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_LONG).show();
+                startActivity(intent);
+            }
+        });
+
+        viewCollectionButton = (Button) findViewById(R.id.viewCollectionButton);
+        viewCollectionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CollectionActivity.class);
+                String temp = "THE CLICK WORKED";
+                Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_LONG).show();
+                startActivity(intent);
+            }
+        });
+
+        searchButton = (Button) findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                String temp = "THE CLICK WORKED";
+                Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_LONG).show();
+                startActivity(intent);
+            }
+        });
+
+        logoutButton = (Button) findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                String temp = "THE CLICK WORKED";
+                Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_LONG).show();
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- Instaderm/app/src/main/java/cis350/instaderm/ViewOnlyHomeActivity.java
+++ Instaderm/app/src/main/java/cis350/instaderm/ViewOnlyHomeActivity.java
@@ -6,19 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class SubmitActivity extends ActionBarActivity {
+public class ViewOnlyHomeActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_submit);
+        setContentView(R.layout.activity_view_only_home);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_submit, menu);
+        getMenuInflater().inflate(R.menu.menu_view_only_home, menu);
         return true;
     }
 
